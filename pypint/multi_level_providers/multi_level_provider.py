7b5619265950084922605de129c7dea602b9e421
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-06-04 13:38:57 +0200

    MultiLevelProvider: adjustments to support AbstractLevel and AbstractLevelTransitioner

--- pypint/multi_level_providers/multi_level_provider.py
+++ pypint/multi_level_providers/multi_level_provider.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 """
-
 .. moduleauthor:: Torbjörn Klatt <t.klatt@fz-juelich.de>
 """
 from collections.abc import Container
@@ -9,8 +8,8 @@ from collections import OrderedDict
 from weakref import ref
 
 from pypint.multi_level_providers.levels.abstract_level import AbstractLevel
-from pypint.multi_level_providers.level_transitioners.i_level_transition_provider import ILevelTransitionProvider
-from pypint.utilities import assert_condition, assert_is_instance, class_name
+from pypint.multi_level_providers.level_transitioners.abstract_level_transitioner import AbstractLevelTransitioner
+from pypint.utilities import assert_condition, assert_is_in, assert_is_instance, class_name
 
 
 class MultiLevelProvider(Container, Sized):
@@ -33,8 +32,8 @@ class MultiLevelProvider(Container, Sized):
         self._levels = []
         self._level_transitioners = {}
 
-    def integrator(self, level):
-        """Accessor for the integrator for the specified level.
+    def level(self, level):
+        """Accessor for a level specified by an index.
 
         Parameters
         ----------
@@ -43,8 +42,8 @@ class MultiLevelProvider(Container, Sized):
 
         Returns
         -------
-        integrator : :py:class:`.QuadratureBase`
-            Stored integrator for given level.
+        integrator : :py:class:`.AbstractLevel`
+            Stored level.
         """
         return self._levels[level]
 
@@ -71,7 +70,7 @@ class MultiLevelProvider(Container, Sized):
 
         See Also
         --------
-        :py:meth:`.ILevelTransitionProvider.prolongate` : for details on prolongation
+        :py:meth:`.AbstractLevelTransitioner.prolongate` : for details on prolongation
         """
         return self._level_transition(coarse_level=coarse_level, fine_level=fine_level).prolongate(coarse_data)
 
@@ -80,15 +79,15 @@ class MultiLevelProvider(Container, Sized):
 
         Parameters
         ----------
-        fine_data : :py:class:`numpy.ndarray`
-            Fine data to restringate.
+        fine_data :
+            Fine data to restrict.
 
-        fine_level : :py:class:`int`
-            Level of the given data to restringate from.
+        fine_level : :py:class:`.AbstractLevel`
+            Level of the given data to restrict from.
 
-        coarse_level : :py:class:`int`
+        coarse_level : :py:class:`.AbstractLevel`
             *(optional)*
-            Coarse level to restringate onto.
+            Coarse level to restrict onto.
             In case it is :py:class:`None` the next coarser level is taken.
 
         Returns
@@ -98,7 +97,7 @@ class MultiLevelProvider(Container, Sized):
 
         See Also
         --------
-        :py:meth:`.ILevelTransitionProvider.restringate` : for details on restringation
+        :py:meth:`.AbstractLevelTransitioner.restrict` : for details on restringation
         """
         return self._level_transition(coarse_level=coarse_level, fine_level=fine_level).restrict(fine_data)
 
@@ -140,32 +139,32 @@ class MultiLevelProvider(Container, Sized):
         self._levels.append(level)
         level.ml_provider = ref(self)
 
-    def add_level_transition(self, transitioner, coarse_level, fine_level):
+    def add_level_transition(self, transitioner, *args, **kwargs):
         """Adds specialized level transitioner for specified levels.
 
         Parameters
         ----------
-        transitioner : :py:class:`.ILevelTransitionProvider`
+        transitioner : :py:class:`.AbstractLevelTransitioner`
             Special level transitioner for specified prolongation and restringation between given coarse and fine level.
-
-        coarse_level : :py:class:`int`
-            Coarse level of the transitioner.
-
-        fine_level : :py:class:`int`
-            Fine level of the transitioner.
 
         Raises
         ------
         ValueError
-            if ``transitioner`` is not an :py:class:`.ILevelTransitionProvider`
+            if ``transitioner`` is not an :py:class:`.AbstractLevelTransitioner`
         """
-        assert_is_instance(transitioner, ILevelTransitionProvider, descriptor="Level Transitioner", checking_obj=self)
+        assert_is_instance(transitioner, AbstractLevelTransitioner, descriptor="Level Transitioner", checking_obj=self)
+
+        _coarse_level = transitioner.coarse_level
+        _fine_level = transitioner.fine_level
+
+        assert_is_in(_coarse_level, self.levels, elem_desc="Coarse Level", list_desc="Levels", checking_obj=self)
+        assert_is_in(_fine_level, self.levels, elem_desc="Fine Level", list_desc="Levels", checking_obj=self)
 
         # extend/initialize level_transition_provider map if necessary
-        if coarse_level not in self._level_transitioners:
-            self._level_transitioners[coarse_level] = {}
+        if _coarse_level not in self._level_transitioners:
+            self._level_transitioners[_coarse_level] = {}
 
-        self._level_transitioners[coarse_level][fine_level] = transitioner
+        self._level_transitioners[_coarse_level][_fine_level] = transitioner
 
     def get_coarser_level(self, level):
         assert_is_instance(level, AbstractLevel, descriptor="Level", checking_obj=self)
@@ -215,38 +214,42 @@ class MultiLevelProvider(Container, Sized):
 
         Parameters
         ----------
-        coarse_level : :py:class:`int`
+        coarse_level : :py:class:`.AbstractLevel`
             Coarse level of the level transitioner.
 
-        fine_level : :py:class:`int`
+        fine_level : :py:class:`.AbstractLevel`
             Fine level of the level transitioner.
 
         Returns
         -------
-        level_transitioner : :py:class:`.ILevelTransitionProvider`
-            Level transition provider to restringate and prolongate between the given coarse and fine level.
+        level_transitioner : :py:class:`.AbstractLevelTransitioner`
+            Level transition provider to restrict and prolongate between the given coarse and fine level.
             In case no specialized transitioner is found, the default one is returned.
 
         Raises
         ------
         ValueError
 
-            * if ``coarse_level`` and ``fine_level`` are :py:class:`None`
+            * if ``coarse_level`` and ``fine_level`` are both :py:class:`None`
             * if ``fine_level`` is :py:class:`None` and ``coarse_level`` is the finest one
             * if ``coarse_level`` is :py:class:`None` and ``fine_level`` is the coarsest one
+            * if ``fine_level`` or ``coarse_level`` are not in this :py:class:`.MultiLevelProvider`
         """
         assert_condition(coarse_level is not None or fine_level is not None,
                          ValueError, message="Either coarse or fine level index must be given", checking_obj=self)
+
         if fine_level is None:
-            fine_level = coarse_level + 1
+            fine_level = self.get_finer_level(coarse_level)
         if coarse_level is None:
-            coarse_level = fine_level - 1
-        assert_condition(fine_level < self.num_levels, ValueError,
-                         message="There is no finer level than given coarse one: {:d}".format(coarse_level),
+            coarse_level = self.get_coarser_level(fine_level)
+
+        assert_condition(fine_level is not None and coarse_level is not None,
+                         ValueError,
+                         message="Either Fine Level or Coarse Level could not be determined: %s, %s"
+                                 % (coarse_level, fine_level),
                          checking_obj=self)
-        assert_condition(coarse_level >= 0, ValueError,
-                         message="There is no coarser level than given fine one: {:d}".format(fine_level),
-                         checking_obj=self)
+        assert_is_in(coarse_level, self, elem_desc="Coarse Level", list_desc="MultiLevelProvider", checking_obj=self)
+        assert_is_in(fine_level, self, elem_desc="Fine Level", list_desc="MultiLevelProvider", checking_obj=self)
 
         if coarse_level in self._level_transitioners and fine_level in self._level_transitioners[coarse_level]:
             return self._level_transitioners[coarse_level][fine_level]
@@ -256,6 +259,26 @@ class MultiLevelProvider(Container, Sized):
 
     def lines_for_log(self):
         _lines = OrderedDict()
+        _lines['Number Levels'] = "%d" % self.num_levels
+        if self.num_levels > 0:
+            _lines['Levels'] = OrderedDict()
+            _lines['Levels']['Coarsest'] = self.coarsest_level.lines_for_log()
+            if self.num_levels > 1:
+                if self.num_levels > 2:
+                    for i in range(1, self.num_levels - 1):
+                        _lines['Levels']['Intermediate'] = self.levels[i].lines_for_log()
+                _lines['Levels']['Finest'] = self.finest_level.lines_for_log()
+        else:
+            _lines['Levels'] = 'na'
+
+        if len(self._level_transitioners) > 0:
+            _lines['Transitioners'] = OrderedDict()
+            for _coarse in self._level_transitioners.keys():
+                for _fine in self._level_transitioners[_coarse].keys():
+                    _lines['Transitioners']["%s <-> %s" % (self.levels.index(_coarse), self.levels.index(_fine))] = \
+                        self._level_transition(_coarse, _fine).lines_for_log()
+        else:
+            _lines['Transitioners'] = 'na'
         return _lines
 
     def __contains__(self, item):
@@ -265,10 +288,10 @@ class MultiLevelProvider(Container, Sized):
             return False
 
     def __len__(self):
-        return len(self._levels)
+        return self.num_levels
 
     def __str__(self):
         return "%s<0x%x>(num_level=%d)" % (class_name(self), id(self), self.num_levels)
 
     def __repr__(self):
-        return "<%s at 0x%x>" % (class_name(self), id(self))
+        return "<%s at 0x%x : num_level=%d>" % (class_name(self), id(self), self.num_levels)
