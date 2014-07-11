902ea14f4aef46b943df7e8f386863fe1a7340ba
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-07-11 09:12:15 +0200

    states: some cleanup and finishing touches

--- pypint/utilities/states/__init__.py
+++ pypint/utilities/states/__init__.py
@@ -2,8 +2,17 @@
 """
 .. moduleauthor:: Torbjörn Klatt <t.klatt@fz-juelich.de>
 """
-from pypint.utilities.states.i_solver_state import ISolverState
+from pypint.utilities.states.node_state import NodeState
+from pypint.utilities.states.state_sequence import StateSequence
+from pypint.utilities.states.mutable_state_sequence import MutableStateSequence
+from pypint.utilities.states.timestep_state import TimeStepState
+from pypint.utilities.states.iteration_state import IterationState
+from pypint.utilities.states.level_state import LevelState
+from pypint.utilities.states.solver_state import SolverState
 from pypint.utilities.states.sdc_solver_state import SdcSolverState
-from pypint.utilities.states.mlsdc_solver_state import MlSdcSolverState
 
-__all__ = ['ISolverState', 'SdcSolverState']
+__all__ = [
+    'NodeState', 'StateSequence', 'MutableStateSequence',
+    'TimeStepState', 'IterationState', 'LevelState', 'SolverState',
+    'SdcSolverState'
+]
